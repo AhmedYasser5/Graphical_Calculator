8ffede4efcee846a59782cb2166dd245d87914ba
Ahmed Yasser <ahmedyasserfarouk6@gmail.com>
2022-08-15 23:30:44 +0200

    added the interface... implemented the interface

---
+++ include/Parser/Implementation.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "Parser/Interface.hpp"
+#include <cstddef>
+#include <stack>
+#include <unordered_map>
+
+namespace Calculator {
+
+class Parser : public ParserInterface<double, std::string, std::string> {
+private:
+  using UnionContainer = UnionContainer<double, std::string, std::string>;
+
+  std::unique_ptr<NumberHandlerInterface<double>> reader;
+  std::unordered_map<std::string, std::unordered_set<std::string>>
+      operatorDependecies;
+  std::vector<UnionContainer> parsedEquation;
+  std::stack<UnionContainer> stacked;
+
+  void readOperator(const std::string &equation, std::size_t &index);
+  void readSigns(const std::string &equation, std::size_t &index);
+  void readNumber(const std::string &equation, std::size_t &index,
+                  const std::unordered_set<std::string> &variables);
+
+public:
+  Parser(std::unique_ptr<NumberHandlerInterface<double>> numberHandler);
+  virtual ~Parser() = default;
+  virtual void parse(const std::string &equation,
+                     const std::unordered_set<std::string> &variables =
+                         {}); // throws different exceptions
+  virtual const std::vector<UnionContainer> &getParsedEquation() const;
+};
+
+} // namespace Calculator
---
+++ src/Parser/Implementation.cpp
@@ -0,0 +1,126 @@
+#include "Parser/Implementation.hpp"
+#include <cctype>
+#include <stdexcept>
+
+using namespace Calculator;
+using std::isalnum;
+using std::isalpha;
+using std::isdigit;
+using std::runtime_error;
+using std::size_t;
+using std::stack;
+using std::string;
+using std::unordered_set;
+using std::vector;
+
+Parser::Parser(std::unique_ptr<NumberHandlerInterface<double>> numberHandler)
+    : reader(std::move(numberHandler)) {
+  operatorDependecies["+"] = {"+", "-", "*", "/", "%", "^"};
+  operatorDependecies["-"] = {"+", "-", "*", "/", "%", "^"};
+  operatorDependecies["*"] = {"*", "/", "%", "^"};
+  operatorDependecies["/"] = {"*", "/", "%", "^"};
+  operatorDependecies["%"] = {"*", "/", "%", "^"};
+  operatorDependecies["^"] = {};
+}
+
+void Parser::readOperator(const string &equation, size_t &index) {
+  string op;
+  while (index < equation.size() &&
+         operatorDependecies.find(op) == operatorDependecies.end() &&
+         !isalnum(equation[index]))
+    op += equation[index++];
+  --index;
+  auto it = operatorDependecies.find(op);
+  if (it == operatorDependecies.end())
+    throw runtime_error("Unknown operator: " + op);
+  auto &dependencies = it->second;
+  while (!stacked.empty() &&
+         dependencies.find(stacked.top().getOperator()) != dependencies.end()) {
+    parsedEquation.emplace_back(stacked.top());
+    stacked.pop();
+  }
+  UnionContainer part;
+  part.updateOperator(op);
+  stacked.push(part);
+}
+
+void Parser::readSigns(const string &equation, size_t &index) {
+  char sign = '+';
+  while (index < equation.size() &&
+         (equation[index] == '+' || equation[index] == '-')) {
+    if (sign == equation[index])
+      sign = '+';
+    else
+      sign = '-';
+    ++index;
+  }
+  --index;
+  if (sign == '+')
+    return;
+  UnionContainer part;
+  part.updateFunction("-");
+  stacked.push(part);
+}
+
+void Parser::readNumber(const string &equation, size_t &index,
+                        const unordered_set<string> &variables) {
+  readSigns(equation, index);
+  ++index;
+  if (index >= equation.size())
+    throw runtime_error(
+        "The equation should end with a number, a variable, or a function");
+  if (!isalnum(equation[index]))
+    throw runtime_error(
+        "Expected a number, a variable, or a function, but found: " +
+        equation.substr(index, 1));
+  if (isalpha(equation[index])) {
+    string var;
+    while (index < equation.size() && isalpha(equation[index]))
+      var += equation[index++];
+    auto it = variables.find(var);
+    if (it == variables.end())
+      throw runtime_error("Variable " + var + " is not defined");
+    UnionContainer part;
+    part.updateFunction(var);
+    parsedEquation.emplace_back(part);
+  } else {
+    string num;
+    while (index < equation.size() &&
+           (isdigit(equation[index]) || equation[index] == '.'))
+      num += equation[index++];
+    UnionContainer part;
+    part.updateNumber(reader->fromString(num));
+    parsedEquation.emplace_back(part);
+  }
+  --index;
+}
+
+void Parser::parse(const string &equation,
+                   const unordered_set<string> &variables) {
+  bool shouldBeOperator = false;
+  for (size_t i = 0; i < equation.size(); i++) {
+    if (std::isblank(equation[i]))
+      continue;
+    while (!stacked.empty() &&
+           stacked.top().getState() == UnionContainer::FUNCTION) {
+      parsedEquation.emplace_back(stacked.top());
+      stacked.pop();
+    }
+    if (shouldBeOperator)
+      readOperator(equation, i);
+    else
+      readNumber(equation, i, variables);
+    shouldBeOperator = !shouldBeOperator;
+  }
+  if (!shouldBeOperator)
+    throw runtime_error(
+        "The equation should end with a number, a variable, or a function");
+  while (!stacked.empty()) {
+    parsedEquation.emplace_back(stacked.top());
+    stacked.pop();
+  }
+}
+
+const vector<Parser::UnionContainer> &Parser::getParsedEquation() const {
+  return parsedEquation;
+}
