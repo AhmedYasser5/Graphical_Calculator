a868ae0e5c6c4507ab711802c6fe61398529008a
Ahmed Yasser <ahmedyasserfarouk6@gmail.com>
2022-08-22 15:48:33 +0200

    more robustness checks

--- testing/src/Number_Handler_Testing.cpp
+++ testing/src/Number_Handler_Testing.cpp
@@ -8,6 +8,8 @@ struct F : public Calculator::NumberHandler {
   ~F() {}
 };
 
+using std::runtime_error;
+
 BOOST_FIXTURE_TEST_SUITE(Number_Handler, F)
 
 #define TOLERANCE 1e-6
@@ -18,11 +20,11 @@ BOOST_AUTO_TEST_CASE(test_case1) {
 }
 
 BOOST_AUTO_TEST_CASE(test_case2) {
-  BOOST_CHECK_THROW(fromString("1.225.35625"), std::runtime_error);
+  BOOST_CHECK_THROW(fromString("1.225.35625"), runtime_error);
 }
 
 BOOST_AUTO_TEST_CASE(test_case3) {
-  BOOST_CHECK_THROW(fromString("12,456"), std::runtime_error);
+  BOOST_CHECK_THROW(fromString("12,456"), runtime_error);
 }
 BOOST_AUTO_TEST_SUITE_END()
 
@@ -42,6 +44,12 @@ BOOST_AUTO_TEST_CASE(test_case2) {
   BOOST_CHECK_CLOSE(add(1.1011000280106, 3.007080501201), 4.1081805292116,
                     TOLERANCE);
 }
+
+BOOST_AUTO_TEST_CASE(test_case3) {
+  BOOST_CHECK_THROW(add(NAN, 1), runtime_error);
+  BOOST_CHECK_THROW(add(1, NAN), runtime_error);
+  BOOST_CHECK_THROW(add(NAN, NAN), runtime_error);
+}
 BOOST_AUTO_TEST_SUITE_END()
 
 BOOST_AUTO_TEST_SUITE(subtract_Tests)
@@ -57,11 +65,23 @@ BOOST_AUTO_TEST_CASE(test_case2) {
 BOOST_AUTO_TEST_CASE(test_case3) {
   BOOST_CHECK_CLOSE(subtract(1e-6, 2e-6), -1e-6, TOLERANCE);
 }
+
+BOOST_AUTO_TEST_CASE(test_case4) {
+  BOOST_CHECK_THROW(subtract(NAN, 1), runtime_error);
+  BOOST_CHECK_THROW(subtract(1, NAN), runtime_error);
+  BOOST_CHECK_THROW(subtract(NAN, NAN), runtime_error);
+}
 BOOST_AUTO_TEST_SUITE_END()
 
 BOOST_AUTO_TEST_SUITE(multiply_Tests)
 BOOST_AUTO_TEST_CASE(test_case1) {
   BOOST_CHECK_CLOSE(multiply(1.000001, 2.000001), 2.000003000001, TOLERANCE);
+}
+
+BOOST_AUTO_TEST_CASE(test_case2) {
+  BOOST_CHECK_THROW(multiply(NAN, 1), runtime_error);
+  BOOST_CHECK_THROW(multiply(1, NAN), runtime_error);
+  BOOST_CHECK_THROW(multiply(NAN, NAN), runtime_error);
 }
 BOOST_AUTO_TEST_SUITE_END()
 
@@ -71,7 +91,13 @@ BOOST_AUTO_TEST_CASE(test_case1) {
 }
 
 BOOST_AUTO_TEST_CASE(test_case2) {
-  BOOST_CHECK_THROW(divide(1e308, 1e-308), std::runtime_error);
+  BOOST_CHECK_THROW(divide(1e308, 1e-308), runtime_error);
+}
+
+BOOST_AUTO_TEST_CASE(test_case3) {
+  BOOST_CHECK_THROW(divide(NAN, 1), runtime_error);
+  BOOST_CHECK_THROW(divide(1, NAN), runtime_error);
+  BOOST_CHECK_THROW(divide(NAN, NAN), runtime_error);
 }
 BOOST_AUTO_TEST_SUITE_END()
 
@@ -89,11 +115,21 @@ BOOST_AUTO_TEST_CASE(test_case3) {
 }
 
 BOOST_AUTO_TEST_CASE(test_case4) {
-  BOOST_CHECK_THROW(power(-4, -0.5), std::runtime_error);
+  BOOST_CHECK_THROW(power(-4, -0.5), runtime_error);
 }
 
 BOOST_AUTO_TEST_CASE(test_case5) {
-  BOOST_CHECK_THROW(power(-32, 0.2), std::runtime_error);
+  BOOST_CHECK_CLOSE(power(-32, 0.2), -2, TOLERANCE);
+}
+
+BOOST_AUTO_TEST_CASE(test_case6) {
+  BOOST_CHECK_CLOSE(power(-32, -0.2), -0.5, TOLERANCE);
+}
+
+BOOST_AUTO_TEST_CASE(test_case7) {
+  BOOST_CHECK_THROW(power(NAN, 1), runtime_error);
+  BOOST_CHECK_THROW(power(1, NAN), runtime_error);
+  BOOST_CHECK_THROW(power(NAN, NAN), runtime_error);
 }
 BOOST_AUTO_TEST_SUITE_END()
 
@@ -103,7 +139,13 @@ BOOST_AUTO_TEST_CASE(test_case1) {
 }
 
 BOOST_AUTO_TEST_CASE(test_case2) {
-  BOOST_CHECK_THROW(modulo(1e308, 0), std::runtime_error);
+  BOOST_CHECK_THROW(modulo(1e308, 0), runtime_error);
+}
+
+BOOST_AUTO_TEST_CASE(test_case3) {
+  BOOST_CHECK_THROW(modulo(NAN, 1), runtime_error);
+  BOOST_CHECK_THROW(modulo(1, NAN), runtime_error);
+  BOOST_CHECK_THROW(modulo(NAN, NAN), runtime_error);
 }
 BOOST_AUTO_TEST_SUITE_END()
 
@@ -111,6 +153,10 @@ BOOST_AUTO_TEST_SUITE(negate_Tests)
 BOOST_AUTO_TEST_CASE(test_case1) {
   BOOST_CHECK_CLOSE(negate(21554.5615100541401), -21554.5615100541401,
                     TOLERANCE);
+}
+
+BOOST_AUTO_TEST_CASE(test_case2) {
+  BOOST_CHECK_THROW(negate(NAN), runtime_error);
 }
 BOOST_AUTO_TEST_SUITE_END()
 
@@ -122,7 +168,13 @@ BOOST_AUTO_TEST_CASE(test_case2) {
 }
 
 BOOST_AUTO_TEST_CASE(test_case3) {
-  BOOST_CHECK_THROW(log(5, -1), std::runtime_error);
+  BOOST_CHECK_THROW(log(5, -1), runtime_error);
+}
+
+BOOST_AUTO_TEST_CASE(test_case4) {
+  BOOST_CHECK_THROW(log(NAN, 1), runtime_error);
+  BOOST_CHECK_THROW(log(1, NAN), runtime_error);
+  BOOST_CHECK_THROW(log(NAN, NAN), runtime_error);
 }
 BOOST_AUTO_TEST_SUITE_END()
 
