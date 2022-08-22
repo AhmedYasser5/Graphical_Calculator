a020e4edaed6a0b646745323d5877a8cfd70616e
Ahmed Yasser <ahmedyasserfarouk6@gmail.com>
2022-08-23 00:42:39 +0200

    added some corner tests for fractional powers to negative numbers

--- testing/src/Number_Handler_Testing.cpp
+++ testing/src/Number_Handler_Testing.cpp
@@ -106,6 +106,7 @@ BOOST_AUTO_TEST_CASE(test_case1) {
   BOOST_CHECK_CLOSE(power(2.0001, 4), 16.0032002400080001, TOLERANCE);
   BOOST_CHECK_CLOSE(power(2.0001, -2), 0.249975000625, TOLERANCE);
   BOOST_CHECK_CLOSE(power(-10, 3), -1e3, TOLERANCE);
+  BOOST_CHECK_CLOSE(power(-10, -4), 1e-4, TOLERANCE);
 }
 
 BOOST_AUTO_TEST_CASE(test_case2) {
@@ -118,8 +119,8 @@ BOOST_AUTO_TEST_CASE(test_case2) {
 BOOST_AUTO_TEST_CASE(test_case3) {
   BOOST_CHECK_CLOSE(power(32, 0.2), 2, TOLERANCE);
   BOOST_CHECK_CLOSE(power(32, -0.2), 0.5, TOLERANCE);
-  BOOST_CHECK_THROW(power(-32, 0.2), runtime_error);
-  BOOST_CHECK_THROW(power(-32, -0.2), runtime_error);
+  BOOST_CHECK_CLOSE(power(-32, 0.2), -2, TOLERANCE);
+  BOOST_CHECK_CLOSE(power(-32, -0.2), -0.5, TOLERANCE);
 }
 
 BOOST_AUTO_TEST_CASE(test_case4) {
@@ -165,17 +166,24 @@ BOOST_AUTO_TEST_CASE(test_case1) {
 BOOST_AUTO_TEST_CASE(test_case2) {
   BOOST_CHECK_THROW(log(-1e20, 10), runtime_error);
   BOOST_CHECK_THROW(log(5, -1), runtime_error);
+  BOOST_CHECK_THROW(log(5, 1), runtime_error);
   BOOST_CHECK_THROW(log(-1e10, -10), runtime_error);
   BOOST_CHECK_THROW(log(-3, 3), runtime_error);
 }
 
 BOOST_AUTO_TEST_CASE(test_case3) {
   BOOST_CHECK_CLOSE(log(1e100, 10), 100, TOLERANCE);
-  BOOST_CHECK_THROW(log(1e4, -10), runtime_error);
-  BOOST_CHECK_THROW(log(-1e3, -10), runtime_error);
+  BOOST_CHECK_CLOSE(log(1e-3, 10), -3, TOLERANCE);
 }
 
 BOOST_AUTO_TEST_CASE(test_case4) {
+  BOOST_CHECK_CLOSE(log(1e4, -10), 4, TOLERANCE);
+  BOOST_CHECK_CLOSE(log(1e-4, -10), -4, TOLERANCE);
+  BOOST_CHECK_CLOSE(log(-1e3, -10), 3, TOLERANCE);
+  BOOST_CHECK_CLOSE(log(-1e-3, -10), -3, TOLERANCE);
+}
+
+BOOST_AUTO_TEST_CASE(test_case5) {
   BOOST_CHECK_THROW(log(NAN, 1), runtime_error);
   BOOST_CHECK_THROW(log(1, NAN), runtime_error);
   BOOST_CHECK_THROW(log(NAN, NAN), runtime_error);
