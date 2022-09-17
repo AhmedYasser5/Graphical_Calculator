19c2d31d8181074a351fea09a53d6ad109596c12
Ahmed Yasser <ahmedyasserfarouk6@gmail.com>
2022-09-17 02:14:39 +0200

    unneeded virtual removed

--- include/Number_Handler/Implementation.hpp
+++ include/Number_Handler/Implementation.hpp
@@ -16,33 +16,29 @@ protected:
   bool isEqual(const NumberType &first, const NumberType &second) const;
 
 public:
-  virtual ~NumberHandler() = default;
-  virtual NumberType
-  fromString(const string &str) const; // throws std::runtime_error
-  virtual string toString(const NumberType &num) const;
-  virtual NumberType
-  add(const NumberType &augend,
-      const NumberType &addend) const; // throws std::runtime_error
-  virtual NumberType
+  NumberType fromString(const string &str) const; // throws std::runtime_error
+  string toString(const NumberType &num) const;
+  NumberType add(const NumberType &augend,
+                 const NumberType &addend) const; // throws std::runtime_error
+  NumberType
   subtract(const NumberType &minuend,
            const NumberType &subtrahend) const; // throws std::runtime_error
-  virtual NumberType
+  NumberType
   multiply(const NumberType &multiplicand,
            const NumberType &multiplier) const; // throws std::runtime_error
-  virtual NumberType
+  NumberType
   divide(const NumberType &dividend,
          const NumberType &divisor) const; // throws std::runtime_error
-  virtual NumberType
+  NumberType
   power(const NumberType &base,
         const NumberType &exponent) const; // throws std::runtime_error
-  virtual NumberType
+  NumberType
   modulo(const NumberType &dividend,
          const NumberType &divisor) const; // throws std::runtime_error
-  virtual NumberType
+  NumberType
   negate(const NumberType &number) const; // throws std::runtime_error
-  virtual NumberType
-  log(const NumberType &power,
-      const NumberType &base) const; // throws std::runtime_error
+  NumberType log(const NumberType &power,
+                 const NumberType &base) const; // throws std::runtime_error
 };
 
 } // namespace Calculator
